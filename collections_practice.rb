14fa71f8f08863a989a999f68c022843f7647e37
Elliott Vargo <vargoelliott@gmail.com>
2021-01-23 20:45:31 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,43 @@
 
+def sort_array_asc(array)
+  array.sort {|a,b| a <=> b}
+end
+
+def sort_array_desc(array)
+  array.sort  {|a,b| -(a <=> b)}
+end
+
+def sort_array_char_count(array)
+  array.sort {|a,b| a.length <=> b.length}
+end
+
+def swap_elements(array)
+  array.sort {|a,b| (a = array[2]) && (b = array[1])}
+end
+
+def reverse_array(array)
+array.reverse
+end
+
+def kesha_maker(array)
+  array.collect {|i| i = "#{i.match/\w{2}/i}$#{i.match/\w*/i}"}
+
+end
+
+def find_a(array)
+
+end
+
+def sum_array(array)
+  array.sum
+end
+
+def add_s(array)
+  array.each_with_index do |item, i|
+    if i != 1
+      array1 = []
+      array1 << item = "#{item}s"
+      return array1
+    end
+  end
+end
